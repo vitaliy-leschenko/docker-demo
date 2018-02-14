379a0ba8c02a611e554b80acd9ba5f417d74a817
Виталий Лещенко <v.leschenko@vitaliy.org>
2018-02-15 01:37:46 +0300

    refactoring

---
+++ src/Prerequisites/prepare.ps1
@@ -0,0 +1,43 @@
+@("rabbit-mq", "mssql-server") | %{
+    $cid = (docker ps --filter "name=$_" -q)
+    if ($cid -ne $null) {
+        Write-Host "Remove $_ container" -ForegroundColor Yellow
+        & docker rm $cid --force
+    }
+}
+
+Write-Host "Building new images..." -ForegroundColor Green
+
+docker build -f .\Prerequisites\MSSQL\Dockerfile -t mssql-server .
+docker build -f .\Prerequisites\RabbitMQ\Dockerfile -t rabbitmq .
+
+Write-Host "Creating MSSQL..." -ForegroundColor Green
+
+$cid = (docker run -d `
+           --name mssql-server `
+           --hostname mssql `
+           --volume "$PWD/volumes/mssql:c:/data" `
+           --publish 1433:1433/tcp `
+           --env "ACCEPT_EULA=Y" `
+           --env "sa_password=nRhtG1c9" `
+           --env "attach_dbs=[{'dbName':'demodb','dbFiles':['C:\\data\\demodb.mdf', 'C:\\data\\demodb_log.ldf']}]" `
+           --restart on-failure `
+           mssql-server)
+
+$ip = (docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $cid)
+Write-Host "MSSQL Server: $ip"
+
+Write-Host "Creating RabbitMQ..." -ForegroundColor Green
+
+$cid = (docker run -d `
+           --name rabbit-mq `
+           --hostname rabbitmq `
+           --volume "$PWD/volumes/rabbitmq-db:c:/RabbitMQ-data/db" `
+           --volume "$PWD/volumes/rabbitmq-log:c:/RabbitMQ-data/log" `
+           --publish 15672:15672 `
+           --restart on-failure `
+           rabbitmq)
+
+$ip = (docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $cid)
+Write-Host "RabbitMQ Server: http://$($ip):15672"
+
--- src/test.ps1
+++ src/test.ps1
@@ -1,4 +1,4 @@
-﻿$cid = (docker ps --filter "status=running" --filter "name=demo.web" -q)
+﻿$cid = (docker ps --filter "status=running" --filter "name=demo-api" -q)
 $ip = (docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $cid)
 
 $obj = @{ comment = (Get-Date).ToString() }
@@ -6,8 +6,15 @@ $json = ConvertTo-Json $obj
 
 $model = Invoke-RestMethod -Method Post -Uri "http://$ip/api/tasks" -Body $json -ContentType application/json
 while (($model.status -eq 0) -or ($model.status -eq 1)) {
-    Write-Host "Status: $($model.status), Progress: $($model.progress)"
+    Write-Progress -Activity "Demo task processing..." -PercentComplete $($model.progress)
 
     $model = Invoke-RestMethod -Method Get -Uri "http://$ip/api/tasks/$($model.id)" -ContentType application/json
+    Start-Sleep -Milliseconds 250
 }
-Write-Host "Status: $($model.status), Progress: $($model.progress)"
+if ($model.status -eq 2) {
+    Write-Host "Task failed" -ForegroundColor Red
+} else {
+    Write-Progress -Activity "Demo task processing..." -PercentComplete 100
+    Write-Host "Task completed" -ForegroundColor Green
+}
+
