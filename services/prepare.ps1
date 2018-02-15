8f0f4062e2b6f992c2ef4655b227d828352e32c4
Виталий Лещенко <v.leschenko@vitaliy.org>
2018-02-15 21:30:10 +0300

    move files outside src

--- services/prepare.ps1
+++ services/prepare.ps1
@@ -8,15 +8,15 @@
 
 Write-Host "Building new images..." -ForegroundColor Green
 
-docker build -f .\Prerequisites\MSSQL\Dockerfile -t mssql-server .
-docker build -f .\Prerequisites\RabbitMQ\Dockerfile -t rabbitmq .
+docker build -f .\MSSQL\Dockerfile -t mssql-server .
+docker build -f .\RabbitMQ\Dockerfile -t rabbitmq .
 
 Write-Host "Creating MSSQL..." -ForegroundColor Green
 
 $cid = (docker run -d `
            --name mssql-server `
            --hostname mssql `
-           --volume "$PWD/volumes/mssql:c:/data" `
+           --volume "$PWD/../volumes/mssql:c:/data" `
            --env "ACCEPT_EULA=Y" `
            --env "sa_password=nRhtG1c9" `
            --env "attach_dbs=[{'dbName':'demodb','dbFiles':['C:\\data\\demodb.mdf', 'C:\\data\\demodb_log.ldf']}]" `
@@ -31,8 +31,8 @@ Write-Host "Creating RabbitMQ..." -ForegroundColor Green
 $cid = (docker run -d `
            --name rabbit-mq `
            --hostname rabbitmq `
-           --volume "$PWD/volumes/rabbitmq-db:c:/RabbitMQ-data/db" `
-           --volume "$PWD/volumes/rabbitmq-log:c:/RabbitMQ-data/log" `
+           --volume "$PWD/../volumes/rabbitmq-db:c:/RabbitMQ-data/db" `
+           --volume "$PWD/../volumes/rabbitmq-log:c:/RabbitMQ-data/log" `
            --restart on-failure `
            rabbitmq)
 
