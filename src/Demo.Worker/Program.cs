d71f040c5e3f9e2a9db236c75c6c96b949b6a012
Виталий Лещенко <v.leschenko@vitaliy.org>
2018-02-15 16:37:46 +0300

    worker

--- src/Demo.Worker/Program.cs
+++ src/Demo.Worker/Program.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,17 +31,34 @@ namespace Demo.Worker
                     channel.QueueDeclare(queue: "tasks", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                     var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += async (model, ea) =>
+                    consumer.Received += (model, ea) =>
                     {
+                        Console.WriteLine("[Start: " + DateTime.UtcNow.ToString("yyyy:MM:dd HH:mm:ss:ffffzzz") + "]");
+                        var watch = new Stopwatch();
+                        watch.Start();
+
                         try
                         {
                             var body = ea.Body;
                             var message = Encoding.UTF8.GetString(body);
-                            await ProcessMessage(message);
-                        }
-                        catch
-                        {
+
+                            Console.WriteLine(message);
+                            ProcessMessage(message).Wait();
                             channel.BasicAck(ea.DeliveryTag, multiple: false);
+
+                            watch.Stop();
+                            Console.WriteLine("[Success]");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("[Fail]");
+                            Console.WriteLine(ex);
+                            throw;
+                        }
+                        finally
+                        {
+                            watch.Stop();
+                            Console.WriteLine("[" + DateTime.UtcNow.ToString("yyyy:MM:dd HH:mm:ss:ffffzzz") + ", " + watch.ElapsedMilliseconds + "ms.]");
                         }
                     };
                     channel.BasicConsume(queue: "tasks", autoAck: false, consumer: consumer);
@@ -74,7 +92,6 @@ namespace Demo.Worker
                     {
                         task.Progress = t;
                         await db.SaveChangesAsync();
-                        await Task.Delay(Math.Min(10, 100 - t));
                     }
 
                     task.Status = WorkerTaskStatus.Completed;
