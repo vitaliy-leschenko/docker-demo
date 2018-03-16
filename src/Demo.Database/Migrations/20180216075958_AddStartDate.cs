0b482ab325fc164e0ff25f675ce41e640b340699
Виталий Лещенко <v.leschenko@vitaliy.org>
2018-03-16 15:06:46 +0300

    added start date

---
+++ src/Demo.Database/Migrations/20180216075958_AddStartDate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Demo.Database.Migrations
+{
+    public partial class AddStartDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Started",
+                table: "Tasks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Started",
+                table: "Tasks");
+        }
+    }
+}
